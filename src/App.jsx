48a73b5809ac7349466b259b678ee5990ba54df1
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-01 16:21:56 -0300

    Configurando CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GlobalStyle from './styles/GlobalStyles'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div> Hello World</div>
+    </div>
+  );
+}
+
+export default App;
