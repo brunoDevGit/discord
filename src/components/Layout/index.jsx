8f05af23a54797533f2a040c6369e781e7dbdaa9
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-18 15:03:24 -0300

    Titulo do Servidor

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Grid } from './styles';
-import ServerList from '../ServerList/index'
+import ServerList from '../ServerList'
+import ServerName from '../ServerName'
 
 const Layout = () => {
   return (
     <Grid>
       <ServerList />
+      <ServerName />
     </Grid>
   )
 }
